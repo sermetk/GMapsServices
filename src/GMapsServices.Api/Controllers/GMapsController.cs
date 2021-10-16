0916e9fa722046d08816a36fc220bd3f3dc6d336
Sermet <sermetkose@gmail.com>
2021-10-17 00:22:39 +0300

    fix api ignorecase

---
+++ src/GMapsServices.Api/Controllers/GMapsController.cs
@@ -0,0 +1,53 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using GMapsServices.Api.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GMapsServices.Api.Controllers
+{
+    public class GMapsController : Controller
+    {
+        private readonly IMapsServices _mapsServices;
+
+        public GMapsController(IMapsServices mapsServices)
+        {
+            _mapsServices = mapsServices;
+        }
+
+        [HttpGet]
+        [Route("/Maps/Route")]
+        public async Task<IActionResult> Route(string origin, string destination, CancellationToken cancellationToken)
+        {
+            var result = await _mapsServices.RouteAsync(origin, destination, cancellationToken);
+
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("/Maps/PlaceDetail")]
+        public async Task<IActionResult> PlaceDetail(string placeId, CancellationToken cancellationToken)
+        {
+            var result = await _mapsServices.PlaceDetailAsync(placeId, cancellationToken);
+
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("/Maps/AutoComplete")]
+        public async Task<IActionResult> AutoComplete(string origin, string input, CancellationToken cancellationToken)
+        {
+            var result = await _mapsServices.AutoCompleteAsync(origin, input, cancellationToken);
+
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("/Maps/ReverseGeocode")]
+        public async Task<IActionResult> ReverseGeocode(string latlng, CancellationToken cancellationToken)
+        {
+            var result = await _mapsServices.ReverseGeocodeAsync(latlng, cancellationToken);
+
+            return Ok(result);
+        }
+    }
+}
---
+++ src/GMapsServices.Api/Startup.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+using GMapsServices.Api.Components;
+using GMapsServices.Api.Filters;
+using GMapsServices.Api.Contracts;
+using GMapsServices.Api.Services;
+using GMapsServices.Api.Models;
+
+namespace GMapsServices.Api
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+
+        private readonly IHostEnvironment _environment;
+
+        public Startup(IConfiguration configuration, IHostEnvironment environment)
+        {
+            _configuration = configuration;
+
+            _environment = environment;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<GoogleMapsOptions>(options => _configuration.GetSection("Maps").Bind(options));
+
+            services.AddTransient(typeof(IMapsServices), typeof(MapsService));
+
+            services.AddTransient(typeof(IExternalHttpRequests), typeof(ExternalHttpRequests));
+
+            services.AddHttpClient();
+
+            services.AddControllers(options => options.Filters.Add(typeof(ServiceResultWrapper)));
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "GMaps Place Services",
+                    Version = "1.0.0"
+                });
+            });
+
+            services.AddDbContextPool<GMapsServicesContext>(options =>
+            {
+                options.UseNpgsql(_configuration.GetConnectionString("Database")).EnableSensitiveDataLogging(_environment.IsDevelopment());
+
+                options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseSerilogRequestLogging(options => options.EnrichDiagnosticContext = (diagnosticContext, httpContext) =>
+            {
+                if (httpContext.Features.Get<IExceptionHandlerPathFeature>() != null)
+                {
+                    diagnosticContext.Set("Exception", httpContext.Features.Get<IExceptionHandlerPathFeature>().Error);
+                }
+            });
+
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "GMapsServices v1");
+
+                c.RoutePrefix = string.Empty;
+            });
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints => endpoints.MapControllers());
+
+            //if (!env.IsEnvironment("Test"))
+            //{
+            //    gmapsServicesContext.Database.EnsureCreated();
+            //}
+        }      
+    }
+}
